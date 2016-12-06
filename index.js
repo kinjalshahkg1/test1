7c34c090a60be74367b0e63e78794fa36685ab8c
kinjalshahkg1 <kinjal.gshah@einfochips.com>
2016-12-06 13:50:07 +0530

    index.js changed

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.type('text/plain'); // set content-type
+  res.send('i am a beautiful butterfly'); // send text response
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+app.listen(process.env.PORT || 4730);
